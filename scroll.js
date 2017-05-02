45ffce56ce75f255a4f739a7cb8b2c731b0295f5
Maia <maia.rt.46@gmail.com>
2017-05-02 18:32:15 -0500

    probando

--- scroll.js
+++ scroll.js
@@ -1,23 +1,13 @@
-	// document.getElementById("toggle").addEventListener("click", function(event){
- //  		event.preventDefault();
- //  			document.getElementById("nav-header").classList.toggle("open");
- // 			 document.getElementById('body').classList.toggle("overflow-hidden");
-	// });
 	var lastScrollTop = 0;
 	window.addEventListener("scroll", function(){
    		var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
    			if (currentScroll > lastScrollTop){
-   				  document.getElementById('nav-background').style.display="none";
-   				  document.getElementById("nav-header").classList.remove("header-in");
-   				  document.getElementById("nav-header").style.opacity = 0;
+   				 document.getElementById("nav-header").classList.add("scrollon");
+                document.getElementsByClassName("lista").style.color= "#333447";
    			} else {
-   				  document.getElementById('nav-background').style.display="table";
-   				  document.getElementById("nav-header").classList.add("header-in");
-   				  document.getElementById("nav-header").classList.add("solid");
-   				  document.getElementById("nav-header").style.opacity = 1;
+   				  document.getElementById("nav-header").classList.add("scrollon");
    				    if(currentScroll<=3){
-   				      document.getElementById("nav-header").classList.remove("solid");
-   				      document.getElementById("nav-header").classList.remove("header-in");
+   				      document.getElementById("nav-header").classList.remove("scrollon");
    				    }
    			}
    				lastScrollTop = currentScroll;
